6b89ffa3c297d01d9fca4c80259108c7dd22a931
Shreyassondur <shreyas.u.sondur@gmail.com>
2026-06-26 04:48:52 +0530

    Fix postgres connect_args issue

--- app/db/session.py
+++ app/db/session.py
@@ -2,7 +2,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from app.core.config import settings
 
+connect_args = {"check_same_thread": False} if settings.SQLALCHEMY_DATABASE_URI.startswith("sqlite") else {}
+
 engine = create_engine(
-    settings.SQLALCHEMY_DATABASE_URI, connect_args={"check_same_thread": False}
+    settings.SQLALCHEMY_DATABASE_URI, connect_args=connect_args
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
